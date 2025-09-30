7c3d975186491911cf66b14dd3aa7e56b665313a
Doome <domedoome9725451@gmail.com>
2025-09-30 23:27:19 +0700

    server

--- kpis-backend/src/server.ts
+++ kpis-backend/src/server.ts
@@ -16,7 +16,7 @@ const port = 3000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173", // ❌ ไม่ใช้ "*"
+    origin: "*", // ❌ ไม่ใช้ "*"
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     credentials: true, // ✅ ต้องมีถ้าใช้ cookies
     exposedHeaders: ["Authorization"], // optional
